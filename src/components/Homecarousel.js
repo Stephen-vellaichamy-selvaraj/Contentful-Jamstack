6fc75c7fb922eefc930992ce9dcece621c774681
Stephen VellaiChamy Selvaraj <stephen.vellaichamyselvaraj@mindtree.com>
2023-11-22 15:48:20 +0000

    test main

--- src/components/Homecarousel.js
+++ src/components/Homecarousel.js
@@ -7,6 +7,7 @@ export default function Homecarousel(props)
   const Carousels = props?.imageSelection
   let [PageNumber, setisPageNumber] = useState(1)
   console.log("CarouselsLengh: " + Carousels?.length)
+  //console.log("testing")
   
   let showButton = null;
   showButton = Carousels && Carousels.length === 1? null : 1
